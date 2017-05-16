90b1030d78fccea411b75f85b49d9d7c3eb9746b
Mikołaj Sikorski <mikolaj.sikorski@itelo.pl>
2017-05-17 00:37:37 +0200

    add langs to voiceApi

--- src/app/talk/chat/chat.component.ts
+++ src/app/talk/chat/chat.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit, Input, Output, EventEmitter, OnChanges, Inject} from '@angular/core';
 
-
 export interface IMessage {
   text: string,
   date: Date,
@@ -25,9 +24,66 @@ export class ChatComponent implements OnInit, OnChanges {
   @Input() query: string;
   @Input() waitForResponse: boolean = false;
   messages: Array<IMessage> = [];
-
   constructor(@Inject('Window') private window: IWindow) {
   }
+
+  // TODO Voice api service
+  languages: Array<String> = [
+    'UK English Female',
+    'UK English Male',
+    'US English Female',
+    'Spanish Female',
+    'French Female',
+    'Deutsch Female',
+    'Italian Female',
+    'Greek Female',
+    'Hungarian Female',
+    'Turkish Female',
+    'Russian Female',
+    'Dutch Female',
+    'Swedish Female',
+    'Norwegian Female',
+    'Japanese Female',
+    'Korean Female',
+    'Chinese Female',
+    'Hindi Female',
+    'Serbian Male',
+    'Croatian Male',
+    'Bosnian Male',
+    'Romanian Male',
+    'Catalan Male',
+    'Australian Female',
+    'Finnish Female',
+    'Afrikaans Male',
+    'Albanian Male',
+    'Arabic Male',
+    'Armenian Male',
+    'Czech Female',
+    'Danish Female',
+    'Esperanto Male',
+    'Hatian Creole Female',
+    'Icelandic Male',
+    'Indonesian Female',
+    'Latin Female',
+    'Latvian Male',
+    'Macedonian Male',
+    'Moldavian Male',
+    'Montenegrin Male',
+    'Polish Female',
+    'Brazilian Portuguese Female',
+    'Portuguese Female',
+    'Serbo-Croatian Male',
+    'Slovak Female',
+    'Spanish Latin American Female',
+    'Swahili Male',
+    'Tamil Male',
+    'Thai Female',
+    'Vietnamese Male',
+    'Welsh Male'
+  ];
+
+  private selectedLang: string;
+
 
   private onSubmit= (value) => {
   this.waitForResponse = true;
@@ -45,8 +101,11 @@ export class ChatComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes) {
     if (this.message) {
-      this.window.responsiveVoice.speak(this.message.text);
-      this.waitForResponse = false;
+      console.log(this.selectedLang)
+      this.window.responsiveVoice.speak(this.message.text, this.selectedLang, {
+        onend: () => {
+          this.waitForResponse = false;
+        }});
       this.messages.push({
         text: this.message.text,
         date: new Date(),
